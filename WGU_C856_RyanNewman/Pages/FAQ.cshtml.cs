e6bff8055a7b341c4b21aef6471b94c91dd0bf93
Ryan Newman <Ryan.N.Newman@Outlook.com>
2023-09-17 23:31:36 -0400

    Add project files.

---
+++ WGU_C856_RyanNewman/Pages/FAQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WGU_C856_RyanNewman.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
